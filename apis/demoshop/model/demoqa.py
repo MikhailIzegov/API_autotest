61d2d218863742a04844b25a9d9df2dfbf15a7ab
MikhailIzegov <mikhail.izegov@yandex.ru>
2024-03-12 04:14:08 +0300

    demoshop_api: add_to_cart tests added

---
+++ apis/demoshop/model/demoqa.py
@@ -0,0 +1,54 @@
+import requests
+
+from requests import sessions
+
+
+class DemoQA:
+    def __init__(self):
+        self.base_url = "https://demowebshop.tricentis.com"
+        self.authorization_cookie = None
+
+    def login(self, email, password):
+        response = requests.post(
+            url=f"{self.base_url}/login",
+            data={"Email": email, "Password": password},
+            allow_redirects=False
+        )
+        self.authorization_cookie = response.cookies.get("NOPCOMMERCE.AUTH")
+        # return self
+
+    def add_to_cart(self, product="31/1/1", count=2):
+        response = None
+        for i in range(count):
+            response = requests.post(
+                url=f"{self.base_url}/addproducttocart/catalog/{product}",
+                cookies={"NOPCOMMERCE.AUTH": self.authorization_cookie}
+            )
+        return response
+
+
+class DemoQAWithSession:
+    def __init__(self):
+        self.base_url = "https://demowebshop.tricentis.com"
+
+    def demoshop_api(self, method, url, **kwargs):
+        with sessions.Session() as session:
+            response = session.request(method=method, url=self.base_url + url, **kwargs)
+        return response
+
+    def login(self, email, password):
+        self.demoshop_api(
+            'post',
+            url="/login",
+            data={"Email": email, "Password": password},
+            allow_redirects=False
+        )
+
+    def add_to_cart(self, product_id="31/1/1", count=2):
+        response = None
+        for i in range(count):
+            response = self.demoshop_api(
+                'post',
+                url=f"/addproducttocart/catalog/{product_id}"
+            )
+        return response
---
+++ apis/demoshop/tests/test_add_to_cart.py
@@ -0,0 +1,54 @@
+import requests
+from allure import step
+
+from apis.demoshop.model.demoqa import DemoQA, DemoQAWithSession
+
+
+def test_add_to_cart_response(login, password):
+    add_to_cart_url = "https://demowebshop.tricentis.com/addproducttocart/catalog/31/1/1"
+    with step("Авторизуемся"):
+        response = requests.post(
+            "https://demowebshop.tricentis.com/login",
+            data={"Email": login, "Password": password},
+            allow_redirects=False
+        )
+    authorization_cookie = response.cookies.get("NOPCOMMERCE.AUTH")
+
+    with step("Добавляем товар в корзину"):
+        result = requests.post(add_to_cart_url, cookies={"NOPCOMMERCE.AUTH": authorization_cookie})
+
+    with step("Проверяем что продукт добавлен"):
+        assert result.json()['success'] is True
+        assert "The product has been added" in result.json()['message']
+
+        """
+    Тут можно написать проверку гибридом, совместив API и UI тесты:
+    assert result.json()["updatetopcartsectionhtml"] == f"({cart_items_number})"
+    , где cart_items_number - это число товаров в корзине, которое возьмем из UI по локатору
+        """
+
+
+def test_add_to_cart_response_with_model(login, password):
+    with step("Авторизуемся"):
+        demoqa = DemoQA()
+        demoqa.login(email=login, password=password)
+
+    with step("Добавляем товар в корзину"):
+        result = demoqa.add_to_cart()
+
+    with step("Проверяем что продукт добавлен"):
+        assert result.json()['success'] is True
+        assert "The product has been added" in result.json()['message']
+
+
+def test_add_to_cart_response_with_model_and_session(login, password):
+    with step("Авторизуемся"):
+        demoqa = DemoQAWithSession()
+        demoqa.login(email=login, password=password)
+
+    with step("Добавляем товар в корзину"):
+        result = demoqa.add_to_cart()
+
+    with step("Проверяем что продукт добавлен"):
+        assert result.json()['success'] is True
+        assert "The product has been added" in result.json()['message']
